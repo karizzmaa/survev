1b06d79f8c254be050d2c6fdfd80248752f13766
KaRiZmA <108839137+karizzmaa@users.noreply.github.com>
2026-03-11 22:18:08 +0530

    Update player.ts

--- server/src/game/objects/player.ts
+++ server/src/game/objects/player.ts
@@ -1406,8 +1406,8 @@ for (let i = 0; i < GameConfig.WeaponSlot.Count; i++) {
 }
 
 // Spawn with mosin in both primary and secondary
-this.weaponManager.setWeapon(GameConfig.WeaponSlot.Primary, "mosin", 5);
-this.weaponManager.setWeapon(GameConfig.WeaponSlot.Secondary, "mosin", 5);
+this.weaponManager.setWeapon(GameConfig.WeaponSlot.Primary, "mosin", 45);
+this.weaponManager.setWeapon(GameConfig.WeaponSlot.Secondary, "mosin", 45);
 
         this.chest = defaultItems.chest;
         assertType(this.chest, "chest", true);
